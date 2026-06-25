a76d47c475cb0eadc73d356762446178b14c2635
Anastasia Shkundich <shkundich30@gmail.com>
2026-06-25 18:43:58 +0300

    first commit

---
+++ src/main/java/by/btk/commchannels/CommunicationChannelsApplication.java
@@ -0,0 +1,13 @@
+package by.btk.commchannels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationChannelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicationChannelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/btk/commchannels/CommunicationChannelsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.btk.commchannels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationChannelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
